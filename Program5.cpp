10a2273b69ed12aa2b38f07bcb6f679e0f32a8c7
Samiyaansarii <162891076+Samiyaansarii@users.noreply.github.com>
2024-11-10 13:02:24 +0530

    Update Program5.cpp

--- Program5.cpp
+++ Program5.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class A;  // Forward declaration
-class B;  // Forward declaration
-class C;  // Forward declaration
+class A;  
+class B;  
+class C;  
 
-// Friend function declaration (defined outside of any class)
 void displayData(const A &a, const B &b, const C &c);
 
 class A {
@@ -18,7 +17,6 @@ protected:
 public:
     A(int pA, int prA) : privateA(pA), protectedA(prA) {}
 
-    // Declare displayData as a friend of A
     friend void displayData(const A &a, const B &b, const C &c);
 };
 
@@ -32,7 +30,6 @@ protected:
 public:
     B(int pB, int prB) : privateB(pB), protectedB(prB) {}
 
-    // Declare displayData as a friend of B
     friend void displayData(const A &a, const B &b, const C &c);
 };
 
@@ -45,12 +42,10 @@ protected:
 
 public:
     C(int pC, int prC) : privateC(pC), protectedC(prC) {}
-
-    // Declare displayData as a friend of C
+ 
     friend void displayData(const A &a, const B &b, const C &c);
 };
 
-// Friend function definition
 void displayData(const A &a, const B &b, const C &c) {
     cout << "Class A - Private: " << a.privateA << ", Protected: " << a.protectedA << endl;
     cout << "Class B - Private: " << b.privateB << ", Protected: " << b.protectedB << endl;
@@ -58,12 +53,10 @@ void displayData(const A &a, const B &b, const C &c) {
 }
 
 int main() {
-    // Creating objects of classes A, B, and C
+    
     A objA(10, 20);
     B objB(30, 40);
     C objC(50, 60);
-
-    // Calling the friend function to access and display data of A, B, and C
     displayData(objA, objB, objC);
 
     return 0;
