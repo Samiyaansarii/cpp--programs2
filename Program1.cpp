2b36d155ca652b29e68ebe51f232a947b1499184
Samiyaansarii <162891076+Samiyaansarii@users.noreply.github.com>
2024-11-10 12:55:10 +0530

    Update Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 class Student {
 public:
-    // Inline function to display the sum of two numbers
     void displaySum(int a, int b) {
         cout << "The sum of " << a << " and " << b << " is: " << (a + b) << endl;
     }
@@ -15,8 +14,7 @@ int main() {
     
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
-
-    // Calling the inline function to display the sum
+    
     student.displaySum(num1, num2);
 
     return 0;
